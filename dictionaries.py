f73dffccd2975e3dd08d2d4cd66c3a453d61fa03
myathtut-zafir <aa.htut@gmail.com>
2024-06-28 22:39:08 +0700

    feat: dict

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+my_dict={'key1':'value1'}
+print(my_dict)
+my_dict_two={'key1':'value1','key2':[0,2,3],"k3":{'inside_key':100}}
+print(my_dict_two['k3'])
+my_dict_two['new_val']=[300]
+print(my_dict_two)
