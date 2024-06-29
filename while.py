be525c876c9f5c507d04009b4e8a88cd9258ca7c
myathtut-zafir <aa.htut@gmail.com>
2024-06-29 15:48:57 +0700

    feat: while loop

---
+++ while.py
@@ -0,0 +1,17 @@
+x=0
+while x <5:
+    print(f'The value of x {x}')
+    x+=1
+else:
+    print("x is not")
+
+y=[0,2,4]
+for num in y:
+    pass
+
+print("out")
+for num in y:
+    if num==2:
+        continue
+    print(num)
+
