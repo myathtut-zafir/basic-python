51330dde868c1a1c0f04c645cb2970d046b34177
myathtut-zafir <aa.htut@gmail.com>
2024-07-05 09:05:40 +0700

    feat:def

---
+++ def.py
@@ -0,0 +1,9 @@
+# function use snake case naming standard
+def testing_function():
+    print("hello")
+
+def testing_function_two(num1,num2):
+    return num1+num2
+
+test=testing_function_two(1,2)
+print(test)
