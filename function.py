33f834be7d731f10542fc9d3052a5141eba3a8f2
myathtut-zafir <aa.htut@gmail.com>
2024-07-07 10:35:45 +0700

    fea:function

---
+++ function.py
@@ -0,0 +1,14 @@
+def say_hello():
+    print("hi")
+
+say_hello()
+
+def say_hello_two(name="mgmmg"):
+    print(f"hi {name} ")
+
+say_hello_two()
+
+def num(num1,num2):
+    return num1+num2
+
+print(num(1,2))
