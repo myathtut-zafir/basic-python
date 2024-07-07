b705c8e8ca325a193a6655c670ce116e2e0f16c1
myathtut-zafir <aa.htut@gmail.com>
2024-07-07 10:58:39 +0700

    feat:tuples function

--- function.py
+++ function.py
@@ -20,3 +20,21 @@ def even_check(number):
         pass
 
 print(even_check(21))
+
+work_hours=[('Abby',100),('Billy',400),('cassie',800)]
+def employee(work_hours):
+     current_max=0
+     employee_of_month=''
+     for employee,hours in work_hours:
+         if hours>current_max:
+             current_max=hours
+             employee_of_month=employee
+         else:
+             pass
+         
+     return (employee_of_month,current_max)
+ 
+print(employee(work_hours))
+name,hour=employee(work_hours) 
+print(name)
+print(hour)
