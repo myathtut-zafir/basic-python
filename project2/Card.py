587fd951d95a0ce7172e91469c1ee723688de6d9
myathtut-zafir <aa.htut@gmail.com>
2024-08-04 21:58:48 +0700

    feat:add card class

---
+++ project2/Card.py
@@ -0,0 +1,17 @@
+import random
+#global varible
+suits=("Hearts","Diamonds","Spades","Clubs")
+ranks=("Two","Three","Four","Five","Six","Seven")
+values={"Two":2,"Three":3}
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit=suit
+        self.rank=rank
+        self.value=values[rank]
+        
+    def __str__(self) -> str:
+        return self.rank+" of "+ self.suit
+
+twoHeart=Card("Hearts","Two")
+print(twoHeart)
