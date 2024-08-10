e78b9fee5e62ee3daa2498c03d39e3ccc539adef
myathtut-zafir <aa.htut@gmail.com>
2024-08-10 14:32:51 +0700

    feat: regrex 2

--- regrex.py
+++ regrex.py
@@ -15,9 +15,26 @@ phone= "my phone is 408-222-1234"
 # ph=re.search('\d\d\d-\d\d\d-\d\d\d\d',phone)
 # ph=ph.group()
 # print(ph)
+#quantifiers
 ph=re.search(r'\d{3}-\d{3}-\d{4}',phone)
 print(ph)
 phone_pattern=re.compile(r'(\d{3})-(\d{3})-(\d{4})')
 result=re.search(phone_pattern,phone)
 print(result.group(1))
-#quantifiers
+
+tex="this cat is there"
+# print(re.search(r'cat|dog',tex))
+print(re.findall(r'...at',tex))
+print(re.findall(r'^\d','1 is number')) # start number
+print(re.findall(r'\d$','The  is number 2')) # end number
+
+phase= 'there are 3 number 34 inside 5 this sentence'
+pattern=r'[^\d]+'
+print(re.findall(pattern,phase))
+
+text="Only find the hypen-words in this sentence. long-ish they"
+pattern=r'\w+-\w+'
+print(re.findall(pattern,text))
+
+
+
