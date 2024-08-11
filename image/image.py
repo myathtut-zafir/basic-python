9566b49629fb8807c8c6468b1cc16cb1cf9833bb
myathtut-zafir <aa.htut@gmail.com>
2024-08-11 10:34:15 +0700

    feat: image

---
+++ image/image.py
@@ -0,0 +1,12 @@
+from PIL import Image # type: ignore
+
+mac=Image.open('1.png')
+print(mac.show())
+# print(mac.size)
+# print(mac.filename)
+# print(mac.paste(100)) # duplicate image
+# print(mac.rezie((3000,500)))
+# crop=mac.crop((0,0,100,100))
+#color
+# color=mac.putalpha(0) # set color on image
+
