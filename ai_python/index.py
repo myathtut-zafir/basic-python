cd8cad3092c78d3c1856987b92f720ebd9c1a7ad
myathtut-zafir <aa.htut@gmail.com>
2024-09-11 21:25:55 +0700

    feat:deepai python basic tutorial

---
+++ ai_python/index.py
@@ -0,0 +1,14 @@
+print("The temperature 75F in degrees Celsius is ((75 - 32) * 5 / 9)C")
+print(f"The temperature 75F in degrees celsius is {(75 - 32) * 5 / 9:.2f} C")
+print(f"dog is  {28/7} olds")
+print(f"dog is  {28/7:.0f} olds")
+print(f"""
+    Most countries use the metric system for recipe measurement, 
+    but American bakers use a different system. For example, they use 
+    fluid ounces to measure liquids instead of milliliters (ml).
+    
+    So you need to convert recipe units to your local measuring system!
+    
+    For example, 8 fluid ounces of milk is {8 * 29.5735} ml.
+    And 100ml of water is {100 / 29.5735} fluid ounces.
+""")
---
+++ ai_python/llm.py
@@ -0,0 +1,8 @@
+# from helper_functions import print_llm_response
+
+driver = "unicorn"
+driver_vehicle = "colorful, asymmetric dinosaur car"
+favorite_planet = "Pluto"
+
+print(f"""Write me a 300 word children's story about a {driver} racing
+a {driver_vehicle} for the {favorite_planet} champion cup.""")
